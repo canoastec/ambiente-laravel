c01b106af658a5eacf243df170b59d1b92f973e7
Jefferson Martins Cardoso <jefferson.cardoso@pmcanoas.rs.gov.br>
2019-08-13 15:36:24 -0300

    Criando projeto

---
+++ src/ServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AmbienteLaravel;
+
+use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+
+class ServiceProvider extends LaravelServiceProvider
+{
+    public function register()
+    {
+
+    }
+
+    public function boot()
+    {
+        
+    }
+}
+?>
