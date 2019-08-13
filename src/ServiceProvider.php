61314374a437db27518dc3d484aab0bf7034ecec
Jefferson Martins Cardoso <jefferson.cardoso@pmcanoas.rs.gov.br>
2019-08-13 16:28:53 -0300

    Implementando ServiceProvider para ajustar rotas e paginator

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,17 +3,35 @@
 namespace AmbienteLaravel;
 
 use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
+use Illuminate\Pagination\Paginator;
 
 class ServiceProvider extends LaravelServiceProvider
 {
     public function register()
     {
-
+		if(env('APP_URL') {
+			$this->handlePaginationUrls()
+		}
     }
+	
+	private function handlePaginationUrls()
+	{
+		Paginator::currentPathResolver(function () {
+            return (env('APP_URL') . '/' . $this->app['request']->path());
+        });
+	}
 
     public function boot()
     {
-        
+        if(env('APP_URL') {
+			$this->handleBaseUrl();
+		}
     }
+	
+	private function handleBaseUrl()
+	{
+		URL::forceRootUrl(env('APP_URL'));
+		URL::forceScheme(str_contains(env('APP_URL'), 'https') ? 'https' : 'http');
+	}
 }
 ?>
